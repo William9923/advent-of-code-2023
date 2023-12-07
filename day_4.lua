1f66922143bd635a93a98cb6f65ab52f0a21576c
William Ong <william.ong@gojek.com>
2023-12-07 23:22:12 +0700

    complete day 4 part 2

--- day_4.lua
+++ day_4.lua
@@ -13,6 +13,11 @@ if not status_ok then
 	print("cannot import libs.collections library")
 end
 
+local status_ok, set = pcall(require, "libs.set")
+if not status_ok then
+	print("cannot import libs.set library")
+end
+
 local status_ok, cpart = pcall(require, "libs.part")
 if not status_ok then
 	print("cannot import libs.part library")
@@ -22,8 +27,8 @@ local part = cpart.get_part()
 print(string.format("Part: %d", part))
 
 function extract(line)
-	local winning_number_raw, game_number_raw = line:match("Card%s*%d+:%s*(%d+[%s%d]+)%s*%|%s*([%s%d]+)")
-	return winning_number_raw:match("^%s*(.-)%s*$"), game_number_raw:match("^%s*(.-)%s*$")
+	local game_num, winning_number_raw, game_number_raw = line:match("Card%s*(%d+):%s*(%d+[%s%d]+)%s*%|%s*([%s%d]+)")
+	return tonumber(game_num), winning_number_raw:match("^%s*(.-)%s*$"), game_number_raw:match("^%s*(.-)%s*$")
 end
 
 function split(s, sep)
@@ -39,7 +44,7 @@ function split(s, sep)
 end
 
 function collect_winning_number(line)
-	local winning_number_raw, game_number_raw = extract(line)
+	local _, winning_number_raw, game_number_raw = extract(line)
 	local winning_numbers = split(winning_number_raw, " ")
 	local game_numbers = split(game_number_raw, " ")
 
@@ -52,6 +57,31 @@ function collect_winning_number(line)
 	end
 
 	return collected_numbers
+end
+
+function collect_total_scratchcard(lines)
+  local combinations = {}
+  for i=1, #lines, 1 do
+    combinations[i] = 1
+  end
+
+  for i, line in ipairs(lines) do
+    local numbers = collect_winning_number(line)
+    local count = #numbers
+
+    for idx = i+1, i + count, 1 do
+      combinations[idx] = combinations[i] + combinations[idx]
+    end
+
+  end
+
+
+  local ans = 0
+  for k, v in ipairs(combinations) do
+    ans = ans + v
+  end
+  return ans
+
 end
 
 if part == "1" then
@@ -68,3 +98,6 @@ if part == "1" then
   print(ans)
 end
 
+if part == "2" then
+  print(collect_total_scratchcard(cio.lines()))
+end
